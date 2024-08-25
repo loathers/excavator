910fd08b6d6ae778f83a84d51efb35117ad01ba4
Joe Kirchoff <Kirchoff.Joseph.P@gmail.com>
2024-08-25 08:48:21 -0700

    migrate data
    i think this is correct

---
+++ packages/excavator-web/prisma/migrations/20240825000000_delete_snowglobe_substats/migration.sql
@@ -0,0 +1,13 @@
+-- Delete substat data from KoL Con 13 snowglobe substat data
+DELETE "SpadingData"
+WHERE
+  "project" = 'KoL Con 13 Snowglobe'
+  AND "data"->>'type' = 'substat'
+
+-- Update remaining data to remove type
+UPDATE "SpadingData"
+SET
+  "data" = "data" - "type"
+WHERE
+  "project" = 'KoL Con 13 Snowglobe'
+  AND "data"->>'type' = 'item'
