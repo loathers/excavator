0085010ff832975cda959fcb9a921f5e20a14cbd
Sam Gaus <sam@gaus.co.uk>
2024-09-30 15:27:51 +0100

    Cascade deletes in snowglobe

--- packages/excavator-web/prisma/migrations/20240825000000_delete_snowglobe_substats/migration.sql
+++ packages/excavator-web/prisma/migrations/20240825000000_delete_snowglobe_substats/migration.sql
@@ -1,8 +1,14 @@
+-- DropForeignKey
+ALTER TABLE "Report" DROP CONSTRAINT "Report_dataId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Report" ADD CONSTRAINT "Report_dataId_fkey" FOREIGN KEY ("dataId") REFERENCES "SpadingData"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
 -- Delete substat data from KoL Con 13 snowglobe substat data
 DELETE FROM "SpadingData"
 WHERE
   "project" = 'KoL Con 13 Snowglobe'
-  AND "data"->>'type' = 'substat'
+  AND "data"->>'type' = 'substat';
 
 -- Update remaining data to remove type
 UPDATE "SpadingData"
@@ -10,4 +16,4 @@ SET
   "data" = "data" - 'type'
 WHERE
   "project" = 'KoL Con 13 Snowglobe'
-  AND "data"->>'type' = 'item'
+  AND "data"->>'type' = 'item';
