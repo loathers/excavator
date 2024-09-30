498d731fabf277c58f259bd424ee33fa7908ecbe
Sam Gaus <sam@gaus.co.uk>
2024-09-30 15:16:12 +0100

    Fix error

--- packages/excavator-web/prisma/migrations/20240825000000_delete_snowglobe_substats/migration.sql
+++ packages/excavator-web/prisma/migrations/20240825000000_delete_snowglobe_substats/migration.sql
@@ -1,5 +1,5 @@
 -- Delete substat data from KoL Con 13 snowglobe substat data
-DELETE "SpadingData"
+DELETE FROM "SpadingData"
 WHERE
   "project" = 'KoL Con 13 Snowglobe'
   AND "data"->>'type' = 'substat'
