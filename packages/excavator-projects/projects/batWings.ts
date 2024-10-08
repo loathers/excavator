e5d00b5e9cbaa1aacf7fd2219b7ee4b2f75a85ab
horrible little slime <69secret69email69@gmail.com>
2024-10-08 15:51:23 -0400

    format

--- packages/excavator-projects/projects/batWings.ts
+++ packages/excavator-projects/projects/batWings.ts
@@ -1,5 +1,15 @@
-import { availableAmount, currentRound, getProperty, haveEquipped, Item, myAscensions, myDaycount, myId, sessionStorage } from "kolmafia";
-import { ExcavatorData, ExcavatorProject } from "../type";
+import {
+  availableAmount,
+  currentRound,
+  getProperty,
+  haveEquipped,
+  Item,
+  myAscensions,
+  myDaycount,
+  myId,
+} from "kolmafia";
+
+import { ExcavatorProject } from "../type";
 
 export const BAT_WINGS: ExcavatorProject = {
   name: "Bat Wings",
@@ -7,9 +17,9 @@ export const BAT_WINGS: ExcavatorProject = {
   author: "sweaty bill",
   since: 28078, //  track remaining bat wing skills
   hooks: {
-    COMBAT_ROUND: spadeBatWings
-  }
-}
+    COMBAT_ROUND: spadeBatWings,
+  },
+};
 
 type BatwingData = {
   userAscensionDay: string; // Used to assemble multiple data points into a single cohesive set
@@ -18,9 +28,7 @@ type BatwingData = {
   encounterWasFree: boolean; // independent of `wingsTriggered`; wings cannot trigger when a fight is natively free
   victory: boolean; // Checks for WINWINWIN
   wingsTriggered: boolean; // Checks if the wings triggerd on this particular combat
-}
-
-
+};
 
 function spadeBatWings(encounter: string, page: string): BatwingData | null {
   if (currentRound() !== 0) return null;
@@ -35,7 +43,6 @@ function spadeBatWings(encounter: string, page: string): BatwingData | null {
     wingsTriggered,
     triggersToday,
     victory: page.includes("WINWINWIN"),
-    encounterWasFree: page.includes('FREEFREEFREE')
-  }
-
+    encounterWasFree: page.includes("FREEFREEFREE"),
+  };
 }
