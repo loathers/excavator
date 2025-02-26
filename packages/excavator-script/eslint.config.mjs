3d109ef3d565edc9a7134f9cc20ee0a40d509873
Sam Gaus <sam@gaus.co.uk>
2025-02-26 13:57:21 +0000

    Prepare for upgrade to react router 7

--- packages/excavator-script/eslint.config.mjs
+++ packages/excavator-script/eslint.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
 import eslint from "@eslint/js";
-import prettier from "eslint-config-prettier";
 import * as libram from "eslint-plugin-libram";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
-  prettier,
   {
     plugins: { libram },
     languageOptions: {
--- packages/excavator-web/vite.config.ts
+++ packages/excavator-web/vite.config.ts
@@ -1,7 +1,25 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 
+declare module "@remix-run/node" {
+  // or cloudflare, deno, etc.
+  interface Future {
+    v3_singleFetch: true;
+  }
+}
+
 export default defineConfig({
-  plugins: [remix()],
+  plugins: [remix(
+    {
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_lazyRouteDiscovery: true,
+        v3_singleFetch: true,
+        v3_routeConfig: true,
+      }
+    }
+  )],
   assetsInclude: ["favicon.png"],
 });
