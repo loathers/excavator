f6c76d4b49b3803240f6780e0ded05cd81276807
Rinn <Kirchoff.Joseph.P@gmail.com>
2025-09-04 05:06:24 +0000

    Build: (59113d6) No taffy in Location.none either

--- scripts/excavator/excavator.js
+++ scripts/excavator/excavator.js
@@ -6821,7 +6821,7 @@ var DROP_PULLED_RED_TAFFY = {
   author: "Rinn",
   hooks: {
     COMBAT_ROUND: function(encounter, page) {
-      if ((0, import_kolmafia13.currentRound)() !== 0) return null;
+      if ((0, import_kolmafia13.currentRound)() !== 0 || (0, import_kolmafia13.myLocation)() === import_kolmafia13.Location.none) return null;
       if ((0, import_kolmafia13.myLocation)().environment === "underwater" || import_kolmafia13.Location.get((0, import_kolmafia13.getProperty)("_seadentWaveZone")) === (0, import_kolmafia13.myLocation)()) {
         var result = page.match(/and gives you an item before disappearing into the murky depths.*?You acquire an item: <b>(.*?)<\/b>/);
         if (!result) return null;
