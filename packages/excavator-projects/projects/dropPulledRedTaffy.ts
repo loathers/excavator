0dee17bb2c575ceb619c775f37c2eb5da63ddb32
Joe Kirchoff <Kirchoff.Joseph.P@gmail.com>
2025-09-03 22:01:07 -0700

    _seadentWaveZone is considered underwater

--- packages/excavator-projects/projects/dropPulledRedTaffy.ts
+++ packages/excavator-projects/projects/dropPulledRedTaffy.ts
@@ -1,4 +1,4 @@
-import { currentRound, myLocation } from "kolmafia";
+import { currentRound, getProperty, Location, myLocation } from "kolmafia";
 
 import { ExcavatorProject } from "../type.js";
 import { toNormalisedItem } from "../utils.js";
@@ -10,14 +10,18 @@ export const DROP_PULLED_RED_TAFFY: ExcavatorProject = {
   hooks: {
     COMBAT_ROUND: (encounter: string, page: string) => {
       if (currentRound() !== 0) return null;
-      // TODO: Does _seadentWaveZone when implemented allow for this?
-      if (myLocation().environment !== "underwater") return null;
-      const result = page.match(
-        /and gives you an item before disappearing into the murky depths.*?You acquire an item: <b>(.*?)<\/b>/,
-      );
-      if (!result) return null;
-      const item = toNormalisedItem(result[1]);
-      return { item };
+      if (
+        myLocation().environment === "underwater" ||
+        Location.get(getProperty("_seadentWaveZone")) === myLocation()
+      ) {
+        const result = page.match(
+          /and gives you an item before disappearing into the murky depths.*?You acquire an item: <b>(.*?)<\/b>/,
+        );
+        if (!result) return null;
+        const item = toNormalisedItem(result[1]);
+        return { item };
+      }
+      return null;
     },
   },
 };
