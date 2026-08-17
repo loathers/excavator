3e4750cc808b2fc793f9c76f2eecd93a2a9e823e
Ignose <ihpares@gmail.com>
2026-08-17 16:04:29 -0400

    Prettier

--- packages/excavator-projects/projects/dropCupOf13s.ts
+++ packages/excavator-projects/projects/dropCupOf13s.ts
@@ -1,15 +1,7 @@
-import {
-  getProperty,
-  Item,
-  myId,
-  myTurncount,
-} from "kolmafia";
+import { getProperty, Item, myId, myTurncount } from "kolmafia";
 
 import { ExcavatorProject } from "../type.js";
-import {
-  isEquippedAtEndOfCombat,
-  toNormalisedItem,
-} from "../utils.js";
+import { isEquippedAtEndOfCombat, toNormalisedItem } from "../utils.js";
 
 export const DROP_CUP_OF_13: ExcavatorProject = {
   name: "Cup of 13s drops",
@@ -17,8 +9,7 @@ export const DROP_CUP_OF_13: ExcavatorProject = {
   author: "Ignose",
   hooks: {
     COMBAT_ROUND: (encounter: string, page: string) => {
-      if (!isEquippedAtEndOfCombat(Item.get("Cup of 13s")))
-        return null;
+      if (!isEquippedAtEndOfCombat(Item.get("Cup of 13s"))) return null;
       const result = page.match(
         /You hear a gurgling from your Cup of 13s.*?You acquire an item: <b>(.*?)<\/b>/,
       );
